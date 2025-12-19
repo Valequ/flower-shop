f69138c6cfe713bc855918c6643302756440a8c8
Valequ <valeriakargova0@gmail.com>
2025-12-19 23:18:39 +0400

    Адаптив под все разрешения экрана

--- static/main/js/js.js
+++ static/main/js/js.js
@@ -6,6 +6,26 @@ function togglePayment() {
 function selectPayment(method) {
     document.getElementById('paymentLabel').textContent = method;
     document.querySelector('.payment-method').classList.remove('open');
-    // Сохраняем выбранный способ оплаты
     localStorage.setItem('paymentMethod', method);
 }
+
+// бургер
+document.addEventListener('DOMContentLoaded', function() {
+    const burgerBtn = document.getElementById('burger-btn');
+    const navMenu = document.getElementById('nav-menu');
+
+    burgerBtn.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+        burgerBtn.classList.toggle('active');
+        
+        document.body.classList.toggle('no-scroll');
+    });
+
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navMenu.classList.remove('active');
+            burgerBtn.classList.remove('active');
+        });
+    });
+});
